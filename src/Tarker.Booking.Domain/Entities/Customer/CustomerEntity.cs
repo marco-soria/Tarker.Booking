9321fdbccded17f3ea3ebac3740268996239c547
marco-soria <marco.soriaa7@gmail.com>
2025-03-18 22:10:04 -0500

    entities development

---
+++ src/Tarker.Booking.Domain/Entities/Customer/CustomerEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarker.Booking.Domain.Entities.Customer
+{
+    public class CustomerEntity
+    {
+        public int CustomerId { get; set; }
+        public string FullName { get; set; }
+        public string DocumentNumber { get; set; }
+   
+    }
+}
