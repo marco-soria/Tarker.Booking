aeae28eca8568d43242135fb526eab25a9af59d7
marco-soria <marco.soriaa7@gmail.com>
2025-03-18 22:28:37 -0500

    deleted unused file

--- src/Tarker.Booking.Domain/Entities/Booking/BookingEntity.cs
+++ src/Tarker.Booking.Domain/Entities/Booking/BookingEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tarker.Booking.Domain.Entities.Booking
+﻿namespace Tarker.Booking.Domain.Entities.Booking
 {
     public class BookingEntity
     {
@@ -16,6 +10,6 @@ namespace Tarker.Booking.Domain.Entities.Booking
 
         public int CustomerId { get; set; }
         public int UserId { get; set; }
-        
+
     }
 }
--- src/Tarker.Booking.Domain/Entities/Customer/CustomerEntity.cs
+++ src/Tarker.Booking.Domain/Entities/Customer/CustomerEntity.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tarker.Booking.Domain.Entities.Customer
+﻿namespace Tarker.Booking.Domain.Entities.Customer
 {
     public class CustomerEntity
     {
         public int CustomerId { get; set; }
         public string FullName { get; set; }
         public string DocumentNumber { get; set; }
-   
+
     }
 }
